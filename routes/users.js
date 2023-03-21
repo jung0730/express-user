b4b3965a6e07180e132d04fae620a60cdd74ce1f
Rebecca <rebecca.yu@senao.com.tw>
2023-03-21 15:47:31 +0800

    Day13 challenge

--- routes/users.js
+++ routes/users.js
@@ -24,16 +24,29 @@ router.post('/', async (req, res, next) => {
     let { nickName, gender, avatar} = data;
     if(!nickName||!gender){
       // 回傳失敗
-
+      res.status(400).json({
+        "status": "false",
+        "message": "使用者資料未填寫完成"
+      })
     } else {
       // 新增至 User model
-    
+      const newUser = await User.create({
+        nickname,
+        gender,
+        avatar
+      })
       // 回傳成功
-
+      res.status(200).json({
+        "status": "success",
+        "data": newUser
+      })
     }
   } catch (error) {
     // 回傳失敗
-
+    res.status(400).json({
+      "status": "false",
+      "message": error.message
+    })
   }
 });
 
@@ -41,32 +54,43 @@ router.post('/', async (req, res, next) => {
 router.patch('/:id', async (req, res, next) => {
   try{
     // 取得 id
-
+    const id = req.params.id;
     const data = req.body;
     let { nickName, gender, avatar } = data;
     if(!nickName||!gender){
       // 回傳失敗 "使用者資料未填寫完整"
-
+      res.status(400).json({
+        "status": "false",
+        "message": "使用者資料未填寫完成"
+      })
     } else {
       const editContent = {
         nickName,
         gender
       } 
       // 找出此筆 id 並編輯資料
-      // const editUser = 
-
+      const editUser = await User.findByIdAndUpdate(id, editContent, { new: true }) // 預設修改成功後 Mongoose 只會回傳修改的資訊，而不會回傳修改成功的資料，如果期望結果是修改成功後會回傳修改成功的資料的話，那麼就將 new 改為 true 即可。
       if (editUser) {
         // 回傳成功
-
+        res.status(200).json({
+          "status": "success",
+          "data": editUser
+        })
       } else {
         // 回傳失敗 "id 不存在"
-
+        res.status(400).json({
+          "status": "false",
+          "message": "id 不存在"
+      });
       }
       
     }
   } catch (error) {
     // 回傳失敗
-
+    res.status(400).json({
+      "status": "false",
+      "message": error.message
+    })
   }
 });
 
